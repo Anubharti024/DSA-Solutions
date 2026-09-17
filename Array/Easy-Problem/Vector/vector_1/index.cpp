32665aebc3b0489dd226b6e776869f924099a702
Anu Bharti <anubharti024@gmail.com>
2026-09-17 16:34:48 +0530

    spiral-matrix-solution

--- Array/Easy-Problem/Vector/vector_1/index.cpp
+++ Array/Easy-Problem/Vector/vector_1/index.cpp
@@ -7,18 +7,16 @@ using namespace std;
 int main()
 {
     // create 2D vector :
-    vector<vector<int>>matrix(3, vector<int>(4,1));
-   
-    // print 
+    vector<vector<int>> matrix(3, vector<int>(4, 1));
+
+    // print
     // for(int i =0; i<3; i++){
     //     for(int j =0; j < 4; j++){
     //         cout << matrix[i][j] <<" ";
     //     }
     // }
 
-
     // find the row and col size in matrix in 2D array:
-    cout<<"Row : "<< matrix.size()<<endl;
-    cout<<"Col : "<< matrix[0].size()<<endl;
-
+    cout << "Row : " << matrix.size() << endl;
+    cout << "Col : " << matrix[0].size() << endl;
 }
